3eb87b77b48fb84afe58633d1d248540956112ae
Obie Fernandez <obiefernandez@gmail.com>
2024-06-26 12:54:06 -0600

    fix specs

--- spec/raix/rails_spec.rb
+++ spec/raix/rails_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Raix::Rails do
+RSpec.describe Raix do
   it "has a version number" do
-    expect(Raix::Rails::VERSION).not_to be nil
+    expect(Raix::VERSION).not_to be nil
   end
 end
