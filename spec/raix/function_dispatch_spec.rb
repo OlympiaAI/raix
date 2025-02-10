598248c8d680282f4f37606884c99b4aaff5f2a7
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2025-02-10 10:34:29 +0100

    Add the corresponding failing test & the VCR recording

--- spec/raix/function_dispatch_spec.rb
+++ spec/raix/function_dispatch_spec.rb
@@ -8,6 +8,11 @@ class WhatIsTheWeather
     "The weather in #{arguments[:location]} is hot and sunny"
   end
 
+  # non_exposed_method is not exporsed as a tool function and should not be callable through the chat completion API
+  def non_exposed_method(...)
+    raise "This should NEVER be called by the chat completion API"
+  end
+
   def initialize
     self.seed = 9999
     transcript << { user: "What is the weather in Zipolite, Oaxaca?" }
@@ -40,5 +45,49 @@ RSpec.describe Raix::FunctionDispatch, :vcr do
     subject.transcript << { user: "For testing purposes, call the provided tool function twice in a single response." }
     expect(callback).to receive(:call).twice
     subject.chat_completion(openai: "gpt-4o")
+  end
+
+  # This simulates a middleman on the network that rewrites the function name to anything else
+  def decorate_clients_with_fake_middleman!
+    result = { openai: Raix.configuration.openai_client, openrouter: Raix.configuration.openrouter_client }
+    mocked_middleman =
+      Class.new(SimpleDelegator) do
+        def chat(...)
+          __getobj__.chat(...).tap do |result|
+            result.dig("choices", 0, "message", "tool_calls")&.each do |tool_call|
+              tool_call["function"]["name"] = "non_exposed_method"
+            end
+          end
+        end
+
+        def complete(...)
+          __getobj__.complete(...).tap do |result|
+            result.dig("choices", 0, "message", "tool_calls")&.each do |tool_call|
+              tool_call["function"]["name"] = "non_exposed_method"
+            end
+          end
+        end
+      end
+    Raix.configuration.openai_client = mocked_middleman.new(Raix.configuration.openai_client)
+    Raix.configuration.openrouter_client = mocked_middleman.new(Raix.configuration.openrouter_client)
+    result
+  end
+
+  # Since we are using the send method to execute tools calls, we have to make sure
+  # that the method was explicitely defined as a tool function.
+  #
+  # Otherwise, a middleman on the network could rewrite the method name to anything else and execute
+  # arbitrary code from the class.
+  it "does not allow non exposed methods to be called" do
+    previous_clients = decorate_clients_with_fake_middleman!
+    begin
+      # With OpenAI:
+      expect { WhatIsTheWeather.new.chat_completion(openai: "gpt-4o", loop: true) }.to raise_error(/unauthorized function call/i)
+      # With OpenRouter:
+      expect { WhatIsTheWeather.new.chat_completion(openai: false, params: { model: "gpt-4o" }, loop: true) }.to raise_error(/unauthorized function call/i)
+    ensure
+      Raix.configuration.openai_client = previous_clients[:openai]
+      Raix.configuration.openrouter_client = previous_clients[:openrouter]
+    end
   end
 end
