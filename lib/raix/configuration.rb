3711d2305b433b3547086b8e5f25505fb3539121
Urban Hafner <contact@urbanhafner.com>
2025-05-23 12:49:54 +0200

    Class level configuration for Raix
    This adds a class level configuration for Raix, allowing users to e.g. set the
    client per class. This is useful if you want to use different API keys for
    different parts of your application.
    
    The code is a bit verbose and the `Raix::Configuration` class doesn't look that
    nice anymore, but I felt that this would be the better choice over doing some
    meta programming to first check the class level config and the fall back to the
    global config. Obviously that is a debatable.
    
    Not sure if everything is in the right place either and how much to test this.

--- lib/raix/configuration.rb
+++ lib/raix/configuration.rb
@@ -5,23 +5,36 @@ module Raix
   class Configuration
     # The temperature option determines the randomness of the generated text.
     # Higher values result in more random output.
-    attr_accessor :temperature
+    def temperature
+      get_with_fallback(:temperature)
+    end
+    attr_writer :temperature, :max_tokens, :max_completion_tokens, :model, :openrouter_client, :openai_client
 
     # The max_tokens option determines the maximum number of tokens to generate.
-    attr_accessor :max_tokens
+    def max_tokens
+      get_with_fallback(:max_tokens)
+    end
 
     # The max_completion_tokens option determines the maximum number of tokens to generate.
-    attr_accessor :max_completion_tokens
+    def max_completion_tokens
+      get_with_fallback(:max_completion_tokens)
+    end
 
     # The model option determines the model to use for text generation. This option
     # is normally set in each class that includes the ChatCompletion module.
-    attr_accessor :model
+    def model
+      get_with_fallback(:model)
+    end
 
-    # The openrouter_client option determines the default client to use for communicatio.
-    attr_accessor :openrouter_client
+    # The openrouter_client option determines the default client to use for communication.
+    def openrouter_client
+      get_with_fallback(:openrouter_client)
+    end
 
     # The openai_client option determines the OpenAI client to use for communication.
-    attr_accessor :openai_client
+    def openai_client
+      get_with_fallback(:openai_client)
+    end
 
     DEFAULT_MAX_TOKENS = 1000
     DEFAULT_MAX_COMPLETION_TOKENS = 16_384
@@ -29,11 +42,24 @@ module Raix
     DEFAULT_TEMPERATURE = 0.0
 
     # Initializes a new instance of the Configuration class with default values.
-    def initialize
+    def initialize(fallback: nil)
       self.temperature = DEFAULT_TEMPERATURE
       self.max_completion_tokens = DEFAULT_MAX_COMPLETION_TOKENS
       self.max_tokens = DEFAULT_MAX_TOKENS
       self.model = DEFAULT_MODEL
+      self.fallback = fallback
+    end
+
+    private
+
+    attr_accessor :fallback
+
+    def get_with_fallback(method)
+      value = instance_variable_get("@#{method}")
+      return value if value
+      return unless fallback
+
+      fallback.public_send(method)
     end
   end
 end
